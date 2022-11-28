1444aab9e97cf566fe8e77d3ed8f5ac4c5333d75
2021113433 <106805163+2021113433@users.noreply.github.com>
2022-11-28 22:47:06 +0800

    First Working Script
    Only bash file, there a lot more functions to do and tests

--- Extraction Tool.py
+++ Extraction Tool.py
@@ -2,6 +2,68 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 
+#####################################################################
+# Get Choice
+#####################################################################
+
+def getchoice():
+
+    menu = """
+These are some of the artifacts that can be extracted instantly!
+
+1. Bash History
+2. Trash Bin
+3. Recently Used Application
+4. Users
+0. Exit
+"""
+    print(menu)
+
+    choice = input("Please enter your choice: ")
+
+    match choice:
+        case "1":
+            getHistory()
+        case "2":
+            getHistory()
+        case "3":
+            getHistory()
+        case "4":
+            getHistory()
+        case "0":
+            quit("\nThank you for using Linux Artifacts Extraction Tool!\n")
+
+#####################################################################
+# Get Bash History
+#####################################################################
+
+def getHistory():
+
+    fileToSearch = ".bash_history"
+    directoryToSearch = "home"
+
+    for relPath,dirs,files in os.walk(rootDirectory):
+        if(directoryToSearch in dirs):
+            targetDirectory = os.path.join(rootDirectory,relPath,directoryToSearch)
+            break
+
+    for relPath,dirs,files in os.walk(targetDirectory):
+        if(fileToSearch in files):
+            targetPath = os.path.join(targetDirectory,relPath,fileToSearch)
+            with open(targetPath, "r") as input:
+                with open("Bash History.txt", "w") as output:
+                    for line in input:
+                        output.write(line)
+    print("\nBash History.txt extracted at: " + currentWorkingDirectory)
+    return getchoice()
+
+#####################################################################
+# Main Program
+#####################################################################
+
+print("\nWelcome! Thank you for using Linux Artifacts Extraction Tool!\n")
+print("\nPlease enter the location path for the mounted image: \n")
+
 root = tk.Tk()
 root.withdraw()
 
@@ -9,21 +71,12 @@ currentWorkingDirectory = os.getcwd()
 rootDirectory = filedialog.askdirectory()
 # dirList = os.listdir(targetDirectory)
 
-print("Current working directory is: " + currentWorkingDirectory)
-print("Root directory for the mounted image is: " + rootDirectory)
+# print("Current working directory is: " + currentWorkingDirectory)
+# print("Root directory for the mounted image is: " + rootDirectory)
 # print(dirList)
 
+getchoice()
 
 #####################################################################
-# Get Bash History
+# END
 #####################################################################
-fileToSearch = ".bash_history"
-
-for relPath,dirs,files in os.walk(rootDirectory):
-    if(fileToSearch in files):
-        targetPath = os.path.join(rootDirectory,relPath,fileToSearch)
-        print("Bash History is at: " + targetPath)
-        with open(targetPath, "r") as input:
-            with open("Bash History.txt", "w") as output:
-                for line in input:
-                    output.write(line)
