42a5d3711637f888a6a5add7dbca4cc20b76674b
2021113433 <106805163+2021113433@users.noreply.github.com>
2022-11-28 21:35:15 +0800

    Search File successful
    Uses os.walk but root directory is still not perfectly defined

--- Extraction Tool.py
+++ Extraction Tool.py
@@ -5,13 +5,25 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
-targetDirectory = filedialog.askopenfilename()
-currentDirectory = os.getcwd()
+currentWorkingDirectory = os.getcwd()
+rootDirectory = filedialog.askdirectory()
+# dirList = os.listdir(targetDirectory)
 
-print("Current working directory is: " + currentDirectory)
-print("Targeted directory is: " + targetDirectory)
+print("Current working directory is: " + currentWorkingDirectory)
+print("Root directory for the mounted image is: " + rootDirectory)
+# print(dirList)
 
-# with open(targetDirectory, "r") as input:
-#     with open("Command History.txt", "w") as output:
-#         for line in input:
-#             output.write(line)
+
+#####################################################################
+# Get Bash History
+#####################################################################
+fileToSearch = ".bash_history"
+
+for relPath,dirs,files in os.walk(rootDirectory):
+    if(fileToSearch in files):
+        targetPath = os.path.join(rootDirectory,relPath,fileToSearch)
+        print("Bash History is at: " + targetPath)
+        with open(targetPath, "r") as input:
+            with open("Bash History.txt", "w") as output:
+                for line in input:
+                    output.write(line)
