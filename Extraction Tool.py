e17f0c312d06b854517091a949ae1bce40c35560
2021113433 <106805163+2021113433@users.noreply.github.com>
2022-12-01 03:47:40 +0800

    User and Group DONE

--- Extraction Tool.py
+++ Extraction Tool.py
@@ -37,7 +37,7 @@ These are some of the artifacts that can be extracted instantly!
         case "3":
             getHistory()
         case "4":
-            return 0
+            getUser()
         case "5":
             return 0
         case "6":
@@ -206,6 +206,77 @@ def getInfo():
 
     print("\nFile OS Information extracted at: " + currentWorkingDirectory)        
               
+    return
+
+#####################################################################
+# Account and Group
+#####################################################################
+
+def getUser():
+
+    etcDirectory = "etc"
+    passwdToSearch = "passwd"
+    groupToSearch = "group"
+
+    # Users
+
+    for relPath,dirs,files in os.walk(rootDirectory):
+        if(etcDirectory in dirs):
+            targetDirectory = os.path.join(rootDirectory,relPath,etcDirectory)
+            break
+
+    for relPath,dirs,files in os.walk(targetDirectory):
+        if(passwdToSearch in files):
+            passwdPath = os.path.join(targetDirectory,relPath,passwdToSearch)
+            break
+        else:
+            passwdPath = False
+    
+    bool(passwdPath)
+    if passwdPath is False:
+        with open("Users.txt", "w") as output:
+            output.write("Extracted from mount point: " + rootDirectory + "\n")
+            output.write("File not found! Try search manually at: /etc/passwd")
+        print("\nFile not found! Try search manually at: /etc/passwd")
+        print("\nFile User and Group extracted at: " + currentWorkingDirectory)
+        return
+
+    with open(passwdPath, "r") as input:
+        with open("Users.txt", "w") as output:
+            output.write("Extracted from mount point: " + rootDirectory + "\n\n")
+            for line in input:
+                output.write(line)
+
+    # group
+
+    for relPath,dirs,files in os.walk(rootDirectory):
+        if(etcDirectory in dirs):
+            targetDirectory = os.path.join(rootDirectory,relPath,etcDirectory)
+            break
+
+    for relPath,dirs,files in os.walk(targetDirectory):
+        if(groupToSearch in files):
+            groupPath = os.path.join(targetDirectory,relPath,groupToSearch)
+            break
+        else:
+            groupPath = False
+    
+    bool(groupPath)
+    if groupPath is False:
+        with open("Groups.txt", "w") as output:
+            output.write("Extracted from mount point: " + rootDirectory + "\n")
+            output.write("File not found! Try search manually at: /etc/group")
+        print("\nFile not found! Try search manually at: /etc/group")
+        print("\nFile User and Group extracted at: " + currentWorkingDirectory)
+        return
+
+    with open(groupPath, "r") as input:
+        with open("Groups.txt", "w") as output:
+            output.write("Extracted from mount point: " + rootDirectory + "\n\n")
+            for line in input:
+                output.write(line)
+
+    print("\nFile User and Group extracted at: " + currentWorkingDirectory)
     return
 
 #####################################################################
